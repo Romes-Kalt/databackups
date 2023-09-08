64ddc75126202e9bcbaf680bfc538f194b20a69a
Roman Kaltschew <96346952+Romes-Kalt@users.noreply.github.com>
2023-09-08 03:01:49 +0200

    file update

--- git_push.bat
+++ git_push.bat
@@ -1,3 +1,9 @@
-git add .
+git add *.json  
 git commit -m "%DATE% %TIME% %1"
-git push
+git push && echo "JSONs pushed"
+git config --global i18n.commitEncoding cp1252
+git add *.csv  
+git commit -m "%DATE% %TIME% %1"
+git push && echo "csv pushed"
+git config --global i18n.commitEncoding utf8
+
