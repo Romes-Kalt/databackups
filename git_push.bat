a798653a0313d9a98c680437acc23d063694ea4c
Roman Kaltschew <96346952+Romes-Kalt@users.noreply.github.com>
2023-09-10 12:31:29 +0200

    10.09.2023 12:31:28,90 push_from_acer

--- git_push.bat
+++ git_push.bat
@@ -1,6 +1,6 @@
 git fetch
 git pull
-Rem python save_in_utf8.py && echo "CSV resaved to utf8"
+python save_in_utf8.py && echo "CSV resaved to utf8"
 git add .  
 git commit -m "%DATE% %TIME% %1"
 git push
