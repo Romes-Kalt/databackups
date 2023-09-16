3e0a1c63d37e7db26d965271424140a5ea429aae
Roman Kaltschew <96346952+Romes-Kalt@users.noreply.github.com>
2023-09-16 07:21:58 +0200

    16.09.2023  7:21:58,87

--- git_push.bat
+++ git_push.bat
@@ -1,3 +1,4 @@
+cp /c/Users/roman/Python/PyCharmProjects/BER_arr_dep/data/flight_data.csv /c/Users/roman/Documents/GitHub/databackups/flight_data.csv
 git fetch
 git pull
 python save_in_utf8.py && echo "CSV resaved to utf8"
