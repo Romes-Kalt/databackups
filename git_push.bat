9e4613776cd9f13ab759fc0dce942c1cc6e9c8e2
Roman Kaltschew <96346952+Romes-Kalt@users.noreply.github.com>
2023-10-21 11:11:49 +0200

    21.10.2023 11:11:49,66 push_from_acer

--- git_push.bat
+++ git_push.bat
@@ -1,7 +1,7 @@
 cp /c/Users/roman/Python/PyCharmProjects/BER_arr_dep/data/flight_data.csv /c/Users/roman/Documents/GitHub/databackups/flight_data.csv
 git fetch
 git pull
-python save_in_utf8.py && echo "CSV resaved to utf8"
+python save_in_utf8.py
 git add .  
 git commit -m "%DATE% %TIME% %1"
 git push
